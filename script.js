c08cdc5c303e316974fb11840b246bb4504df828
Gopikrishna-M-A <gopikrishna6003@gmail.com>
2020-09-30 02:48:40 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function clickGreen(){
+    document.getElementById("main").style.backgroundColor="green";
+
+}
+function clickRed(){
+    document.getElementById("main").style.backgroundColor="red";
+    
+}
+function clickBlue(){
+    document.getElementById("main").style.backgroundColor="blue";
+    
+}
+function clickclear(){
+    document.getElementById("main").style.backgroundColor="black";
+    
+}
